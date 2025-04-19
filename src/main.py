f68ad3d9f90cf0fc837b234da595eda852053852
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-04-19 21:26:25 +0800

    feat: allow all hosts at CORS

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 from fastapi import FastAPI
 from yookassa import Configuration
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes import router
 from core import config
@@ -8,6 +9,17 @@ from core import config
 app = FastAPI()
 app.include_router(router)
 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @lru_cache
 def get_settings():
