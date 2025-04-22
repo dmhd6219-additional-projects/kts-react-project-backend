4c73ee8884d080233bcbf3b6e1687affa1d48c77
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-04-22 22:04:54 +0800

    fix: types

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from fastapi import APIRouter, HTTPException
 
 from models.payment import PaymentRequest
-from models.user import UserLoginRequest
+from models.user import UserLoginRequest, UserLoginResponse
 from models.order import OrderRequest
 
 from services.payment_service import create_payment_yookassa
@@ -10,14 +12,14 @@ from services.auth_service import authenticate_user, create_user, save_order, ge
 router = APIRouter()
 
 
-@router.post("/login")
+@router.post("/login", response_model=UserLoginResponse)
 def login(user: UserLoginRequest):
     if authenticate_user(user.username, user.password):
         return {"message": "Login successful", "user_id": user.username}
     raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
-@router.post("/register", status_code=201)
+@router.post("/register", status_code=201, response_model=UserLoginResponse)
 def register(user: UserLoginRequest):
     if not authenticate_user(user.username, user.password):
         created_user = create_user(user.username, user.password)
@@ -25,7 +27,7 @@ def register(user: UserLoginRequest):
     raise HTTPException(status_code=409, detail="User already exists")
 
 
-@router.get("/orders")
+@router.get("/orders", response_model=List[int])
 def orders(user_id: str):
     return get_orders(user_id)
 
--- src/models/user.py
+++ src/models/user.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class UserLoginRequest(BaseModel):
     username: str
     password: str
+
+class UserLoginResponse(BaseModel):
+    message: str
+    user_id: str
